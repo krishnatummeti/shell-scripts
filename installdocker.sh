83b09fa2e75bef10fcad88a6c5ee2d1fa4ebc92a
Krishna T <krishnatummeti@gmail.com>
2025-06-14 16:20:31 +0530

    adding docker install script

--- installdocker.sh
+++ installdocker.sh
@@ -17,14 +17,12 @@ PLATFORM=$(uname -s)_$ARCH
 
 # docker install 
 
-sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker ec2-user 
-sudo newgrp docker
-echo "User added to docker group"
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
 
 
 # Install Docker Compose (latest as of May 2025)
@@ -33,10 +31,11 @@ sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version > /tmp/docker-compose_version.txt
 
 
+
 # kubectl install
 
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
-chmod +x ./kubectl
+sudo chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/
 kubectl version --client > /tmp/kubectl_version.txt
 
@@ -45,5 +44,5 @@ kubectl version --client > /tmp/kubectl_version.txt
 
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-sudo mv /tmp/eksctl /usr/local/bin
+mv /tmp/eksctl /usr/local/bin
 eksctl version > /tmp/eksctl_version.txt
