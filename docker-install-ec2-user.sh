80fec94866aeb1966a379abb7fdd449ec3fd376d
Krishna T <krishnatummeti@gmail.com>
2025-06-17 18:21:34 +0530

    Add docker install files

--- docker-install-ec2-user.sh
+++ docker-install-ec2-user.sh
@@ -22,13 +22,13 @@ sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version > /tmp/docker-compose_version.txt
 
 # kubectl install
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mv kubectl /usr/local/bin/
-kubectl version --client > /tmp/kubectl_version.txt
+# curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+# sudo chmod +x ./kubectl
+# sudo mv kubectl /usr/local/bin/
+# kubectl version --client > /tmp/kubectl_version.txt
 
 # eksctl install
-curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-mv /tmp/eksctl /usr/local/bin
-eksctl version > /tmp/eksctl_version.txt
+# curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+# tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+# mv /tmp/eksctl /usr/local/bin
+# eksctl version > /tmp/eksctl_version.txt
